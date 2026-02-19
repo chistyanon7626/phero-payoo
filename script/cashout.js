282e7cb3e1b2da240abd88284ec71538b48c054f
chistyanon7626 <canon7626@gmail.com>
2026-02-20 00:31:47 +0600

    cashout lay out done adding js code

---
+++ script/cashout.js
@@ -0,0 +1,6 @@
+document.getElementById("cashout").addEventListener("click",function(){
+    const cashoutNumberInput = document.getElementById("Agent-number");
+    const cashoutNumber = cashoutNumberInput.value;
+    console.log(cashoutNumber);
+
+})
