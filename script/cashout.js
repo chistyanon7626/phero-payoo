7cead8482d513c36f885a6f3954575950f44a90c
chistyanon7626 <canon7626@gmail.com>
2026-02-20 14:41:18 +0600

    js for cashout page

--- script/cashout.js
+++ script/cashout.js
@@ -1,6 +1,32 @@
 document.getElementById("cashout").addEventListener("click",function(){
     const cashoutNumberInput = document.getElementById("Agent-number");
     const cashoutNumber = cashoutNumberInput.value;
-    console.log(cashoutNumber);
+    if(cashoutNumber.length != 11){
+        alert("Invalid Number");
+        return;
+    }
 
+ const cashoutAmountInput = document.getElementById("amount",);
+ const cashoutAmount = cashoutAmountInput.value;   
+ 
+ const balanceElement = document.getElementById("balance");
+ const balance = balanceElement.innerText;
+ 
+ const newBalance = Number(balance) - Number(cashoutAmount);
+ if(newBalance < 0){
+    alert("Invalid Amount");
+    return;
+ }
+ const cashoutPinInput = document.getElementById("input-pin");
+ const pin = cashoutPinInput.value;
+ if ( pin === "1234"){
+    alert("Cashout successful")
+    balanceElement.innerText=newBalance;
+
+
+ }
+ else{
+    alert("Invalid pin");
+    return;
+ }
 })
